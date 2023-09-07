da03f632fad83d7a48bbaa0606eeac79e8d2b320
valeyeranyy <valeria.carrascoa@alumno.buap.mx>
2023-09-07 14:57:20 -0600

    ArreglosSemanales

---
+++ clases iniciales/GeneradorDeNombres/GeneradorDeNombresDeVillanos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneradorDeNombres
+{
+    class GeneradorDeNombresDeVillanos
+    {
+        public string[] descripciones = {"Vengativo", "oscuro", "rojo"};
+        public string[] tipoDeCriaturas = { "gargola", "gnomo", "criatura" };
+        public string[] origen = { "del este", "del oeste", "del sur" };
+    }
+}
